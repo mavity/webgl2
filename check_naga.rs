19322599f61da546a7b5ea0635aeaf6ef984a176
Oleg Mihailik <mihailik@gmail.com>
2025-12-29 09:53:30 +0000

    Stupis files in the root

--- check_naga.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    // This is just to check compilation
-    let _ = naga::AddressSpace::Function;
-    let _ = naga::AddressSpace::Private;
-    let _ = naga::AddressSpace::Uniform;
-    let _ = naga::AddressSpace::Handle;
-}
