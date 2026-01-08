a824d117d05ef6cbccbee2f7c3869dd0c9861476
Oleg Mihailik <mihailik@gmail.com>
2026-01-08 21:16:27 +0000

    Frag constant colour test fix.

--- test/frag_constant_color.test.js
+++ test/frag_constant_color.test.js
@@ -6,7 +6,7 @@ test('Fragment constant color output', async () => {
   const gl = await webGL2();
   try {
     gl.viewport(0, 0, 64, 64);
-    const vs = `#version 300 es\nvoid main(){ gl_Position = vec4(0.0); gl_PointSize = 1.0; }`;
+    const vs = `#version 300 es\nvoid main(){ gl_Position = vec4(0.0, 0.0, 0.0, 1.0); gl_PointSize = 1.0; }`;
     const fs = `#version 300 es\nprecision highp float; out vec4 fragColor; void main(){ fragColor = vec4(0.0,1.0,0.0,1.0); }`;
     const s_vs = gl.createShader(gl.VERTEX_SHADER); gl.shaderSource(s_vs, vs); gl.compileShader(s_vs); assert.ok(gl.getShaderParameter(s_vs, gl.COMPILE_STATUS));
     const s_fs = gl.createShader(gl.FRAGMENT_SHADER); gl.shaderSource(s_fs, fs); gl.compileShader(s_fs); assert.ok(gl.getShaderParameter(s_fs, gl.COMPILE_STATUS));
